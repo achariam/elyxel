6c0b3fe0b7b6da5ad2ac24bc89dce201bc1544ad
achariam <matthew.achariam@gmail.com>
2016-05-28 00:59:07 -0400

    config for deploying, added exrm for releases, new bash script for automating stuff

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Elyxel.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 1.0.5"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
