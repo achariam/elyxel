ff84bbbf55fd6ec116223886b2d774e865a61a39
achariam <matthew.achariam@gmail.com>
2016-06-15 20:56:09 -0400

    new router stuff

--- web/router.ex
+++ web/router.ex
@@ -7,20 +7,34 @@ defmodule Elyxel.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
+    plug Openmaize.Authenticate
   end
 
   scope "/", Elyxel do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
+    get "/confirm", PageController, :confirm
+    get "/askreset", PageController, :askreset
+    post "/askreset", PageController, :askreset_password
+    get "/reset", PageController, :reset
+    post "/reset", PageController, :reset_password
+    get "/login", PageController, :login, as: :login
+    post "/login", PageController, :login_user, as: :login
+    delete "/logout", PageController, :logout, as: :logout
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Elyxel do
-  #   pipe_through :api
-  # end
+  scope "/users", Elyxel do
+    pipe_through :browser
+
+    resources "/", UserController, only: [:index, :show, :edit, :update]
+  end
+
+  scope "/admin", Elyxel do
+    pipe_through :browser
+
+    get "/", AdminController, :index
+    resources "/users", AdminController, only: [:new, :create, :delete]
+  end
+
 end
