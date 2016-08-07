51800340dbe9b154f5642b8a44312e376edf2fb2
achariam <matthew.achariam@gmail.com>
2016-08-06 20:21:00 -0400

    big push to get e-mails working and some title case cleanup

--- lib/elyxel/mailer.ex
+++ lib/elyxel/mailer.ex
@@ -7,32 +7,32 @@ defmodule Elyxel.Mailer do
   """
 
   use Mailgun.Client, domain: Application.get_env(:elyxel, :mailgun_domain),
-                      key:    Application.get_env(:elyxel, :mailgun_key),
-                      mode: :test, # these two lines are only for testing
-                      test_file_path: "tmp/mailgun.json"
+                      key:    Application.get_env(:elyxel, :mailgun_key)
+                      #mode: :test,   #these two lines are for testing
+                      #test_file_path: "tmp/mailgun.json"
 
-  @from "norwegianblue@example.com"
+  @from "hello@mail.elyxel.com"
 
   @doc """
   An email with a confirmation link in it.
   """
   def ask_confirm(email, link) do
-    confirm_url = "http://www.example.com/confirm?#{link}"
+    confirm_url = "https://www.elyxel.com/confirm?#{link}"
     send_email to: email,
-              from: @from,
-              subject: "Request confirmation",
-              html: Phoenix.View.render_to_string(Elyxel.EmailView, "ask_confirm.html", %{confirm_url: confirm_url})
+               from: @from,
+               subject: "Request confirmation",
+               html: Phoenix.View.render_to_string(Elyxel.EmailView, "ask_confirm.html", %{confirm_url: confirm_url})
   end
 
   @doc """
   An email with a link to reset the password.
   """
   def ask_reset(email, link) do
-    confirm_url = "http://www.example.com/reset?#{link}"
+    confirm_url = "https://www.elyxel.com/reset?#{link}"
     send_email to: email,
-    from: @from,
-    subject: "Reset password",
-    html: Phoenix.View.render_to_string(Elyxel.EmailView, "ask_reset.html", %{confirm_url: confirm_url})
+               from: @from,
+               subject: "Reset password",
+               html: Phoenix.View.render_to_string(Elyxel.EmailView, "ask_reset.html", %{confirm_url: confirm_url})
   end
 
   @doc """
@@ -40,8 +40,8 @@ defmodule Elyxel.Mailer do
   """
   def receipt_confirm(email) do
     send_email to: email,
-              from: @from,
-              subject: "Successful confirmation",
-              html: Phoenix.View.render_to_string(Elyxel.EmailView, "receipt_confirm.html", %{})
+               from: @from,
+               subject: "Successful confirmation",
+               html: Phoenix.View.render_to_string(Elyxel.EmailView, "receipt_confirm.html", %{})
   end
 end
