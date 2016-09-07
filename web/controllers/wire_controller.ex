dc1c77ef840f815570d0ab61167414478e3d542f
achariam <matthew.achariam@gmail.com>
2016-09-06 20:58:28 -0400

    add pagination to elyxel

--- web/controllers/wire_controller.ex
+++ web/controllers/wire_controller.ex
@@ -2,12 +2,27 @@ defmodule Elyxel.WireController do
   use Elyxel.Web, :controller
 
   import Elyxel.Authorize
-  alias Elyxel.{User, Wire}
+  import Elyxel.Pagination
+  import Ecto.Query
+  alias Elyxel.Wire
 
 	def action(conn, _), do: auth_action_role conn, ["admin", "user"], __MODULE__
 
+	def index(conn, %{"p" => page}, _user) do
+	  wires =
+	  	Wire
+	  	|> order_by(desc: :inserted_at)
+	  	|> preload([:user])
+	  	|> page(page: page, per_page: 5)
+	  render(conn, "index.html", wires: wires)
+	end
+
 	def index(conn, _params, _user) do
-	  wires = Wire |> Repo.all |> Repo.preload([:user])
+	  wires =
+	  	Wire
+	  	|> order_by(desc: :inserted_at)
+	  	|> preload([:user])
+	  	|> page(page: 0, per_page: 5)
 	  render(conn, "index.html", wires: wires)
 	end
 end
---
+++ web/helpers/pagination.ex
@@ -0,0 +1,38 @@
+defmodule Elyxel.Pagination do
+  @moduledoc """
+  Generic ecto pagination helper
+  """
+  import Ecto.Query
+  alias Elyxel.Repo
+
+  def page(query, page: page, per_page: per_page) do
+    scrub_page = page |> scrub
+    count = per_page + 1
+    result = query
+              |> limit(^count)
+              |> offset(^(scrub_page*per_page))
+              |> Repo.all
+    %{ has_next?: (length(result) == count),
+       has_prev?: scrub_page > 0,
+       list: Enum.slice(result, 0, count-1) }
+  end
+
+  defp scrub(page) do
+    cond do
+      is_integer(page) && (page >= 0) ->
+        page
+      is_binary(page) ->
+        case Integer.parse(page) do
+          {page, _} ->
+            if (page < 0) do
+              0
+            else
+              page
+            end
+          :error -> 0
+        end
+      true ->
+        0
+    end
+  end
+end
